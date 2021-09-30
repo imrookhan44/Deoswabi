c305b12826e51b004dd388e75ca0387cbc7dc41c
Sameerahmadkhan <samerrkaan9@gmail.com>
2021-09-30 04:36:20 -0700

    signup almost done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 // import logo from './logo.svg';
 import Signup from '../src/Component/Signup/Signup';
+import Signin from './Signin/Signin';
 import './App.css';
 
 function App() {
   return (
     <div >
      <Signup/>
+     <Signin/>
     </div>
   );
 }
---
+++ src/Signin/Signin.js
@@ -0,0 +1,10 @@
+import React from 'react'
+// import "../Signin/Signin.css"
+
+export default function Signin() {
+    return (
+        <div>
+            <h1>hello world</h1>
+        </div>
+    )
+}
