6f0c7bd7a6ad7efcd9472f0ad24192239c54ef72
Sameerahmadkhan <samerrkaan9@gmail.com>
2021-10-02 23:07:52 -0700

    almost done signup and signin page

--- src/Component/Signup/Signup.js
+++ src/Component/Signup/Signup.js
@@ -9,13 +9,13 @@ export default function Signup() {
         <div className="row mt-5 ">
             <div className="col-sm-3 offset-3">
                 <h3 className="signup mt-5">SignUp</h3>
-                <input  type='text' className='form-control pt-4' placeholder='Full Name'></input>
+                <input type='text' className='form-control pt-4' placeholder='Full Name'></input>
                 <input type="text" className="form-control pt-4" placeholder="Email"></input>
                 <input type="text" className="form-control pt-4" placeholder='Phone Number'></input>
                 <input type="text" className="form-control pt-4" placeholder="Password" />
                 <input type="text" className="form-control pt-4" placeholder="Repeat Password" />
                 <button type="button" className="btn btn-primary mt-4">Register</button>
-                <p className=' mt-2'>Already have an account <span><a href='/'> Signin </a></span></p>
+                <p className=' mt-2'>Already have an account <a href='/'> Signin </a></p>
                 
                 
             </div>
