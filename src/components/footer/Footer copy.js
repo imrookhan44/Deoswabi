75f92ecc43b06b088c8159d61eeca2f7c2bec7e3
imadik21 <imadik968@gmail.com>
2021-10-14 13:14:32 +0500

    changes in navbar

---
+++ src/components/footer/Footer copy.js
@@ -0,0 +1,14 @@
+import React from 'react'
+const Footer = () => {
+    return (
+        <>
+            <div className='fixed-bottom bg-dark text-primary' >
+                <div className='row mt-3'>
+                    <div className='text-center'><p>Education Fund © 2021</p></div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Footer
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,12 +1,41 @@
 import React from 'react'
-import slider1 from "../../assets/images/money2.jpg";
+import madik from '../../assets/images/education1.jpg'
+import im1 from '../../assets/images/money1.jpg'
+import im2 from '../../assets/images/money2.jpeg'
+import './Home.css'
+import Navbar from "../navbar/Navbar"
+import slider1 from "../../assets/images/education1.jpg";
 
 export default function Home() {
     return (
-        <div className='container-fluid'> 
-            <div className='row'>
-            <img src={slider1} className='img-fluid' alt="slider" />
-            </div>
-        </div>
+        // <div className='container-fluid'> 
+           
+        
+        //    <img src={slider1} className='img-fluid' />        
+                
+            
+        // </div>
+
+        <div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src={madik} class="d-block w-100" alt="..."/>
+    </div>
+    <div class="carousel-item">
+      <img src={im1} class="d-block w-100" alt=""/>
+    </div>
+    <div class="carousel-item">
+      <img src={im2} class="d-block w-100" alt="..."/>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
     )
 }
