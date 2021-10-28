37b1256a7e7c87c832ce8bc358d3d11c46ec27db
imadik21 <imadik968@gmail.com>
2021-10-28 16:16:51 +0500

    changes in easypaisa App in login page

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,6 +1,7 @@
 import React from 'react'
+import { Formik, Form, Field } from 'formik';
 import './Login.css'
-// import login from '../../assets/images/login.jpg'
+import login from '../../assets/images/login.jpg'
 import human from '../../assets/images/human.jpg'
 
 export default function Login() {
@@ -11,6 +12,67 @@ export default function Login() {
             <div className='container-fluid'>
                 <div className='row'>
                     <div className='col-sm-4'>
+                         {/* <Formik initialValues={{name:"",password:"",checkbox:""}} onSubmit={(values) => {
+                             console.log(values);
+                         }}>
+                         <Form>
+                         {/* <lable>Name:</lable> */}
+                             {/* <Field name="name" type="text" placeholder="Username" />
+                             <br /><br />
+                             
+                             {/* <lable>Password:</lable> */}
+                             {/* <Field name="password" type="password"  placeholder="Password" />
+                             <br /><br />
+                             <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike"></input> <b>Remember Username</b> <br /><br />
+                             <button type="submit">Submit</button>
+                         </Form>
+
+                        </Formik>
+ */} 
+&nbsp;
+&nbsp;
+&nbsp;
+&nbsp;
+&nbsp;
+&nbsp;
+&nbsp;
+&nbsp;
+&nbsp;
+&nbsp;
+&nbsp;
+&nbsp;
+&nbsp;
+&nbsp;
+&nbsp;
+&nbsp;
+&nbsp;
+&nbsp;
+&nbsp;
+&nbsp;
+&nbsp;
+&nbsp;
+&nbsp;
+&nbsp;
+&nbsp;
+&nbsp;
+&nbsp;
+&nbsp;
+&nbsp;
+&nbsp;
+&nbsp;
+&nbsp;
+ <img className='img-fluid' src={login}/>
+ 
+ <br /><br /><br /><br />
+
+ <h2>
+                        APCA Education
+                        <br />
+                        </h2>
+                        <h3>
+                      All Pakistan Clerk Assocition
+                    </h3> 
+
                         <form >
                             <input className="border" type="text" placeholder="Username" style={{ outline: "none", borderRadius: "20px", width: "70%", padding: "12px 20px", margin: "8px 0px", display: "inline-block", border: "1px solid #ccc", boxsizing: "border-box", backgroundcolor: "#f1f1f1" }}></input><br />
                             <input className="border" type="password" placeholder="Password" style={{ outline: "none", borderRadius: "20px", width: "70%", padding: "12px 20px", margin: "8px 0px", display: "inline-block", border: "1px solid #ccc", boxsizing: "border-box", backgroundcolor: "#f1f1f1" }}></input><br /><br />
