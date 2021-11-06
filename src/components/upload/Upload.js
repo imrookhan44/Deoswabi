294fd5ac9050c82219eb2b839b7694485c873aaa
imadik21 <imadik968@gmail.com>
2021-11-06 16:40:38 +0500

    done

--- src/components/upload/Upload.js
+++ src/components/upload/Upload.js
@@ -6,27 +6,25 @@ import upload from '../../assets/images/upload.jpeg'
 export default function Upload() {
     return (
        
-        <div className="container2">
+        
 
 <div class="container2">
 
 <img className="img22" src= {upload}>
                         </img> 
-                       
-  <button className="btnnn">Upload a file</button>
-  <input type="file" name="myfile" />
+                     
 </div>
-{/* 
+/* 
             <center>
                 <div class="upload-btn-wrapper mt-5">
                     <button class="btnnn " >Upload a file</button>
                     <input type="file" name="myfile" />
                 </div>
-            </center> */}
-            {/* <div className="img">
+            </center> */
+            /* <div className="img">
                         <img src= {login}>
                         </img>
-                    </div> */}
-        </div>
+                    </div> */
+        
     )
 }
