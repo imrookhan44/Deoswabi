aabdd1a2a2e458aa44ba4d27ba0440c0d1364026
mustafazeb381-123 <mustafazeb381@gmail.com>
2021-11-14 14:52:29 +0500

    buttnDone

--- src/components/Download/Download.js
+++ src/components/Download/Download.js
@@ -1,5 +1,9 @@
 import React,{ useState } from 'react'
 import { Nav, Modal} from 'react-bootstrap';
+import { Button,NavDropdown,Navbar,Container,FormControl,Form, } from 'react-bootstrap';
+import logo from "../../assets/images/upaise-logo.jpg";
+import { Link } from 'react-router-dom'
+
 
 
 function Download() {
@@ -7,15 +11,37 @@ function Download() {
 
     return (
         <div>
-           /* <div className='row mt-4'> 
+           {/* <div className='row mt-4'> 
         <div className='col-sm-6 offset-3'>
         <div class="file btn btn-lg btn-primary">
-             File Upload
-            <input type="file" name="file" className="input-Admin" />
+            File Upload
+        <input type="file" name="file" className="input-Admin" />
         </div>
         </div>
-        </div> */
+        </div>  */}
+
+<Navbar bg="white" expand="lg" >
+  <Container fluid>
+    <Navbar.Toggle aria-controls="navbarScroll" />
+    <Navbar.Collapse id="navbarScroll">
+    <Link className="navbar-brand" to="/"/><img style={{ height: '70px' }} src={logo} alt='logo' />
+      <Nav className="me-auto my-2 my-lg-0 "style={{ maxHeight: '100px' }}navbarScroll>
+        <Nav.Link href="#action1">Home</Nav.Link>
+        <Nav.Link href="#action2">History</Nav.Link>
+        <Nav.Link href="#action1">Sing Up</Nav.Link>
+        <Nav.Link href="#action2">Login</Nav.Link>
+        <Nav.Link href="#action1">Upload</Nav.Link>
+        <Nav.Link href="#action2">user Details</Nav.Link>
+        <Nav.Link href="#action1">Download</Nav.Link>
+        <Nav.Link href="#action2">Admin</Nav.Link>
+      </Nav>
+    </Navbar.Collapse>
+  </Container>
+</Navbar>
+        
         </div>
+
+        
     )
 }
 
