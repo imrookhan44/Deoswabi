a4c9ffa1b95086b85295c55f9222be8d18280e3c
mustafazeb381-123 <mustafazeb381@gmail.com>
2021-11-17 15:59:32 +0500

    done

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,44 +1,44 @@
 import React, { Component, component } from 'react'
 import firebase from '../firebase'
 export class Admin extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state={
-            files : null
+        this.state = {
+            files: null
         }
 
     }
-    handleChange =(files) =>{
+    handleChange = (files) => {
         this.setState({
-            files : files
+            files: files
 
         })
     }
-    handleSave =()=>{
+    handleSave = () => {
         let bucketName = 'images'
         let files = this.state.files[0]
-        let storageRef= firebase.storage().ref(`${bucketName}/${files.name}`)
+        let storageRef = firebase.storage().ref(`${bucketName}/${files.name}`)
         let UploadTask = storageRef.put(files)
         UploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED,
-            ()=>{
-            let downloadURL = UploadTask.snapshot.downloadURL
-        })
+            () => {
+                let downloadURL = UploadTask.snapshot.downloadURL
+            })
     }
-    showImage=()=>{
+    showImage = () => {
         let storageRef = firebase.storage().ref()
-        let spaceRef = storageRef.child('images/'+this.state.files[0].name)
-        storageRef.child('images/'+this.state.files[0].name).getDownloadURL().then((url)=>{
+        let spaceRef = storageRef.child('images/' + this.state.files[0].name)
+        storageRef.child('images/' + this.state.files[0].name).getDownloadURL().then((url) => {
             console.log(url)
-            document.getElementById('new-img').src= url
+            document.getElementById('new-img').src = url
         })
     }
     render() {
         return (
             <div>
-<input type="file" onChange={(e)=>{this.handleChange(e.target.files)}}/>
-<button onClick={this.handleSave}>Save</button>
-<button onClick={this.showImage}>Show image</button>
-<img id="new-img"/>
+                <input type="file" onChange={(e) => { this.handleChange(e.target.files) }} />
+                <button onClick={this.handleSave}>Save</button>
+                <button onClick={this.showImage}>Show image</button>
+                <img id="new-img" />
 
             </div>
         )
