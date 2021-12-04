db2631d5e6c93dfdb0ad7682f08634caf73c5153
imrookhan <khannihar921@gmail.com>
2021-12-04 10:03:54 +0500

    done

--- src/components/user details/user.js
+++ src/components/user details/user.js
@@ -10,60 +10,10 @@ import * as yup from "yup";
 
 toast.configure();
 
-const validationSchema = yup.object().shape({
-  fullName: yup
-    .string()
-    .required("Name is required.")
-    .min(4, "Minimum 4 characters required"),
 
-  Password: yup
-    .string()
-    .required("password is required.")
-    .min(6, "Minimum 6 characters required"),
-  CNIC: yup
-    .string()
-    .required("Enter your CNIC")
-    .min(13, "Minimum 3 characters required"),
-  email: yup.string().required("Enter valid Email"),
-
-  mobileNumber: yup
-    .string()
-    .required("Enter valid phone")
-    .min(10, "minimum 10 characters required"),
-  dateOfBirth: yup.string().required("Enter Your DateOfBirth"),
-  qualification: yup.string().required("Required"),
-  station: yup.string().required("Required"),
-  address: yup.string().required("Required"),
-  appointment: yup.string().required("Required"),
-});
 
 const User = () => {
-  const formik = useFormik({
-    initialValues: {
-      firstName: "",
-      password: "",
-      cnic: "",
-      email: "",
-      mobileNumber: "",
-      dateOfBirth: "",
-      qualification: "",
-      station: "",
-      address: "",
-      appointment: "",
-    },
-    onSubmit: (values) => {
-      alert("Form submitted");
-    },
-    validationSchema: validationSchema,
-  });
-
-  const renderErrorMessage = (field) => {
-    return (
-      formik.touched[field] && (
-        <div class="text-error">{formik.errors[field]}</div>
-      )
-    );
-  };
+  
   const notify = () => toast("Form Successfully Submitted");
 
   const [fullName, setFullName] = useState("");
@@ -128,7 +78,7 @@ const User = () => {
           <form
             autoComplete="off"
             className="form offset-3"
-            onSubmit={(handleSubmit, formik.handleSubmit)}
+            onSubmit={(handleSubmit)}
           >
             <input
               value={fullName}
@@ -137,9 +87,9 @@ const User = () => {
               name="name"
               className="form-control"
               onChange={(e) => setFullName(e.target.value)}
-              {...formik.getFieldProps("fullName")}
+              
             />
-            {renderErrorMessage("fullName")}
+          
 
             <input
               value={password}
@@ -147,12 +97,12 @@ const User = () => {
               placeholder="Password"
               name="pass"
               className="form-control"
-              {...formik.getFieldProps("Password")}
+              
               onChange={(e) => setPassword(e.target.value)}
             />
-            {renderErrorMessage("Password")}
+      
             <input
-              {...formik.getFieldProps("CNIC")}
+             
               value={cnic}
               type="number"
               placeholder="CNIC"
@@ -160,27 +110,27 @@ const User = () => {
               name="pass"
               onChange={(e) => setCnic(e.target.value)}
             />
-            {renderErrorMessage("CNIC")}
+            
             <input
-              {...formik.getFieldProps("email")}
+            
               value={email}
               type="email"
               placeholder="Email Address"
               className="form-control"
               onChange={(e) => setEmail(e.target.value)}
             />
-            {renderErrorMessage("email")}
+
             <input
-              {...formik.getFieldProps("mobile")}
+       
               value={mobile}
               type="number"
               placeholder="Mobile number"
               className="form-control"
               onChange={(e) => setMobile(e.target.value)}
             />
-            {renderErrorMessage("mobile")}
+           
             <input
-              {...formik.getFieldProps("dateOfBirth")}
+
               value={dateOfBirth}
               type="date"
               id="birthday"
@@ -189,9 +139,9 @@ const User = () => {
               name="pass"
               onChange={(e) => setDateOfBirth(e.target.value)}
             />
-            {renderErrorMessage("dateOfBirth")}
+            
             <input
-              {...formik.getFieldProps("qualification")}
+             
               value={qualification}
               type="Qualification"
               placeholder="Qualification"
@@ -199,40 +149,40 @@ const User = () => {
               name="pass"
               onChange={(e) => setQualification(e.target.value)}
             />
-            {renderErrorMessage("qualification")}
+           
             <input
               value={station}
-              {...formik.getFieldProps("station")}
+         
               type="text"
               placeholder="Station"
               className="form-control"
               name="pass"
               onChange={(e) => setStation(e.target.value)}
             />
-            {renderErrorMessage("station")}
+ 
             <input
-              {...formik.getFieldProps("address")}
+             
               value={address}
               type="Address"
               placeholder="Address"
               className="form-control"
               onChange={(e) => setAddress(e.target.value)}
             />
-            {renderErrorMessage("address")}
+         
             <input
-              {...formik.getFieldProps("appointment")}
+            
               value={firstAppointment}
               type="text"
               placeholder="First Appointment"
               className="form-control"
               onChange={(e) => setAppointment(e.target.value)}
             />
-            {renderErrorMessage("appointment")}
+            
             <br />
             <button
               className="btn-primary  control-button "
               id="bTN"
-              onClick={(notify, handleSubmit)}
+              onClick={(notify)}
               style={{ background: loader ? "#ccc" : " rgb(2, 2, 110)" }}
             >
               Submit
