c176672d55a50a33a7f5a1c5305ef57d8ba95875
Nasirali9 <nasiralics99@gmail.com>
2022-01-06 12:44:49 +0500

    done

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -4,8 +4,11 @@ import BugdetData from "../../School Status/BugdetData";
 const Home=()=> {
     return (
         <div>
+            <p>This is Home</p>
             <Contacts/>
+
             <BugdetData/>
+
         </div>
     )
 }
