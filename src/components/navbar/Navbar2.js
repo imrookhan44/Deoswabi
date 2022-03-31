126666b57e5caf98eabd45ca319beef1381f1495
@ihteshamu11 <ihteshamu11@gmail.com>
2022-03-31 11:08:26 +0500

    2nd navber changing

--- src/components/navbar/Navbar2.js
+++ src/components/navbar/Navbar2.js
@@ -7,15 +7,16 @@ function Navbar2() {
     let history = useHistory()
     return (
         <div className='navbar-n2'>
-            <div className='row-N1 mt-2'>
+           
+            <div className='row-N2'>
+            <div className='col-2-n2 mt-2'>
                 <img onClick={() => history.push("/")}
                     style={{ height: "80px", width: "80px" }}
                     src={logo}
                     alt="logo"
                 />
             </div>
-            <div className='row-N2'>
-                <div className="rowfirst" >    بسم الله الرحمن الرحيم
+                <div className="col-10 rowfirst">    بسم الله الرحمن الرحيم
                 </div>
             </div>
         </div>
